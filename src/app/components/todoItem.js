780637e87e872a14cd9aa251cec20312aa719bb9
M. Shaheer <tecxra1003@gmail.com>
2024-05-10 17:54:47 +0500

    model mask

--- src/app/components/todoItem.js
+++ src/app/components/todoItem.js
@@ -1,12 +1,12 @@
 "use client"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Modal } from 'antd';
 import AddTodo from "./addTodo"
 import { faPenToSquare, faTrashCan, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import DeleteTodo from "../components/Dialouge"
 import { Table, Tag } from 'antd';
+import Modal from 'react-modal'
 const { Column } = Table;
 
 
@@ -61,9 +61,25 @@ export default function item({ data, setMessage, id }) {
         setId(iid)
     }
 
+    const customStyles = {
+        overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.1)',
+            alignItems: "center"
+        },
+        content: {
+            backgroundColor: "white",
+            maxWidth: "fit-content",
+            height: "fit-content",
+            top: "10%",
+            left: "40%",
+            border: "3px solid blue",
+            borderRadius: "30px"
+
+        }
+    }
 
     return (
-        <Table dataSource={data} pagination={false} className="z-5 " >
+        <Table dataSource={data} pagination={false} className="z-5 "  >
 
             <Column title="Title" dataIndex="title" key="title" render={(title) => (
                 <>
@@ -118,13 +134,13 @@ export default function item({ data, setMessage, id }) {
                     <div className="flex flex-row-reverse justify-around">
 
                         <button className="mx-1" onClick={() => { idDelete(record._id) }}><FontAwesomeIcon style={{ fontSize: "25px" }} icon={faTrashCan}></FontAwesomeIcon></button>
-                        <Modal open={isOpenDelete} onCancel={() => setIsOpenDelete(false)} mask={false} footer={false}  >
+                        <Modal isOpen={isOpenDelete} onRequestClose={() => setIsOpenDelete(false)} style={customStyles}   >
                             <DeleteTodo task={"Todo"} id={iid} Deleted={Deleted} deleteTodo={deleteTodo} isOpenDelete={isOpenDelete} setIsOpenDelete={setIsOpenDelete} />
 
                         </Modal>
 
                         <button className="mx-1" onClick={() => { idUpdate(record._id) }}><FontAwesomeIcon style={{ fontSize: "25px" }} icon={faPenToSquare}></FontAwesomeIcon></button>
-                        <Modal open={isOpen} onCancel={() => setIsOpen(false)} mask={false} footer={false}  >
+                        <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} style={customStyles}>
                             <AddTodo task={"Update"} id={iid} onSubmit={handleSubmit} />
 
                         </Modal>
